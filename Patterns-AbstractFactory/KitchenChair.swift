357b1a06bb60f2135b6e74fd56ac6df74798c4e4
Rus <de.mr44@gmail.com>
2022-01-02 00:18:46 +0500

    Added some kitchen furniture

---
+++ Patterns-AbstractFactory/KitchenChair.swift
@@ -0,0 +1,13 @@
+//
+//  KitchenChair.swift
+//  Patterns-AbstractFactory
+//
+//  Created by Ruslan on 01.01.2022.
+//
+
+import Foundation
+
+class KitchenChair: Chair {
+    var name: String = "Chair"
+    var type: String = "Kitchen"
+}
---
+++ Patterns-AbstractFactory/KitchenTable.swift
@@ -0,0 +1,13 @@
+//
+//  KitchenTable.swift
+//  Patterns-AbstractFactory
+//
+//  Created by Ruslan on 01.01.2022.
+//
+
+import Foundation
+
+class KitchenTable: Table {
+    var name: String = "Table"
+    var type: String = "Kitchen"
+}
