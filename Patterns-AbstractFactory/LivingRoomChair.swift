68858f66f8e03b923f96a95c97946faf3f087be4
Rus <de.mr44@gmail.com>
2022-01-02 00:27:32 +0500

    Added some living room furniture

---
+++ Patterns-AbstractFactory/LivingRoomChair.swift
@@ -0,0 +1,13 @@
+//
+//  LivingRoomChair.swift
+//  Patterns-AbstractFactory
+//
+//  Created by Ruslan on 02.01.2022.
+//
+
+import Foundation
+
+class LivingRoomChair: Chair {
+    var name: String = "Chair"
+    var type: String = "Living Room"
+}
---
+++ Patterns-AbstractFactory/LivingRoomSofa.swift
@@ -0,0 +1,13 @@
+//
+//  LivingRoomSofa.swift
+//  Patterns-AbstractFactory
+//
+//  Created by Ruslan on 02.01.2022.
+//
+
+import Foundation
+
+class LivingRoomSofa: Sofa {
+    var name: String = "Sofa"
+    var type: String = "Living Room"
+}
